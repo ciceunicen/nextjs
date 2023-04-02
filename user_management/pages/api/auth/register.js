406110dcc2c48947794d8fc633901522263911a0
JuanMolfese <juanjosemolfese@gmail.com>
2023-04-01 23:14:05 -0300

    se agrega en api/register verificacion de mail ya ingresado en DB

--- user_management/pages/api/auth/register.js
+++ user_management/pages/api/auth/register.js
@@ -16,34 +16,34 @@ const saveUser = async (req, res) => {
     
     const { email, password, name, surname} = req.body;
 
-    /* if (!email || !password) {
+    if (!email || !password) {
         res.status(400).json({
         error: "Falta email o password",
         });
         return;
-    } */
-    try{        
-        /* const result = await connection.query("SELECT * FROM user WHERE email = ?", [email]);
-        if (result.length > 0){
-            res.status(409).json({
-                error: "Email ya esta siendo utilizado",
-              });
-        }
-        else { */
-        const salt = bcryptjs.genSaltSync(10);
-        const hash = bcryptjs.hashSync(password, salt);
+    }
 
-        const result = await connection.query("INSERT INTO user ( email, password, name, surname, role) VALUES (?, ?, ?, ?, ?)",
-            [ email, hash, name, surname, 2 ]);            
-        
-        return res.status(200).json({ ...req.body, id: result.insertId });
-        
-    }catch (error) {
-        return res.status(500).json({ message: error.message });
-    }     
+    const verifExistMail = await connection.query("SELECT * FROM user WHERE email = ?", [email]);
+    if (verifExistMail.length > 0){
+        res.status(409).json({
+            error: "Email ya esta siendo utilizado",
+        });
+    }
+    else {    
+        try{ 
+            const salt = bcryptjs.genSaltSync(10);
+            const hash = bcryptjs.hashSync(password, salt);
 
-}
-   
+            const result = await connection.query("INSERT INTO user ( email, password, name, surname, role) VALUES (?, ?, ?, ?, ?)",
+                [ email, hash, name, surname, 2 ]);            
+            
+            return res.status(200).json({ ...req.body, id: result.insertId });
+            
+        }catch (error) {
+            return res.status(500).json({ message: error.message });
+        }     
+    }
+}   
 
 
 
